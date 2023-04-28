ef350aa256a3e884807cd05b9fdc34485ea26094
zooyaho <jiwoo1268@naver.com>
2023-04-28 16:05:37 +0900

    feat: 코치 아이템 컴포넌트 작업

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,16 +6,25 @@
       <router-link to="/register">코치 등록</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coache in filteredCoaches" :key="coache.id">
-        {{ coache.firstName }}
-      </li>
+      <CoachItem
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></CoachItem>
     </ul>
     <h3 v-else>코치가 존재하지 않습니다.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -27,4 +36,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
