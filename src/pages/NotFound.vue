9aecf67a4ccccda57927584b7d5d3f65e24305a2
zooyaho <jiwoo1268@naver.com>
2023-04-28 13:49:43 +0900

    feat: 라우팅 세팅

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>페이지를 찾을 수 없습니다.</h2>
+</template>
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const route = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [{ path: '/contact', component: ContactCoach }], // coaches/c1/contact
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default route;
