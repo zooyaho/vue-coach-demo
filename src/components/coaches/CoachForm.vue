de2d466c9c27ec8ca8050586b17698674d5bd896
zooyaho <jiwoo1268@naver.com>
2023-05-01 21:12:33 +0900

    feat: 코치 등록 폼 유효성 검사 추가

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,38 +1,82 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !firstName.isValid }">
       <label for="firstName">FirstName</label>
-      <input type="text" id="firstName" v-model.trim="firstName" />
+      <input
+        type="text"
+        id="firstName"
+        v-model.trim="firstName.val"
+        @blur="clearValidity('firstName')"
+      />
+      <p v-if="!firstName.isValid">첫 번째 이름은 빈 값일 수 없습니다.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !lastName.isValid }">
       <label for="lastName">LastName</label>
-      <input type="text" id="lastName" v-model.trim="lastName" />
+      <input
+        type="text"
+        id="lastName"
+        v-model.trim="lastName.val"
+        @blur="clearValidity('lastName')"
+      />
+      <p v-if="!lastName.isValid">두 번째 이름은 빈 값일 수 없습니다.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !description.isValid }">
       <label for="description">Description</label>
-      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+      <textarea
+        id="description"
+        rows="5"
+        v-model.trim="description.val"
+        @blur="clearValidity('description')"
+      ></textarea>
+      <p v-if="!description.isValid">설명은 빈 값일 수 없습니다.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !rate.isValid }">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="rate" />
+      <input
+        type="number"
+        id="rate"
+        v-model.number="rate.val"
+        @blur="clearValidity('rate')"
+      />
+      <p v-if="!rate.isValid">시간당 수강료는 0이상여야 합니다.</p>
     </div>
     <!-- s: 자신 있는 전문 분야 -->
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !areas.isValid }">
       <h3>자신 있는 전문 분야</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <input
+          type="checkbox"
+          @blur="clearValidity('areas')"
+          id="frontend"
+          value="frontend"
+          v-model="areas.val"
+        />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <input
+          type="checkbox"
+          @blur="clearValidity('areas')"
+          id="backend"
+          value="backend"
+          v-model="areas.val"
+        />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <input
+          type="checkbox"
+          @blur="clearValidity('areas')"
+          id="career"
+          value="career"
+          v-model="areas.val"
+        />
         <label for="career">Career Advisory</label>
       </div>
+      <p v-if="!areas.isValid">전문 분야가 하나는 있어야 합니다.</p>
     </div>
     <!-- e: 자신 있는 전문 분야 -->
+    <p v-if="!formIsValid">양식을 다시 작성하고 제출해주세요.</p>
     <base-button>Register</base-button>
   </form>
 </template>
@@ -42,21 +86,72 @@ export default {
   emits: ['save-data'],
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      description: '',
-      rate: null,
-      areas: [],
+      firstName: {
+        val: '',
+        isValid: true,
+      },
+      lastName: {
+        val: '',
+        isValid: true,
+      },
+      description: {
+        val: '',
+        isValid: true,
+      },
+      rate: {
+        val: null,
+        isValid: true,
+      },
+      areas: {
+        val: [],
+        isValid: true,
+      },
+      formIsValid: true, // 입력값이 하나라도 실패하면 false
     };
   },
   methods: {
+    clearValidity(input) {
+      // 유효성 검사 실패 후 폼작성 시 원상태 되돌리는 함수
+      // input이 포커스를 잃을 때마다 해당 함수를 실행시킨다.
+      this[input].isValid = true;
+    },
+    validateForm() {
+      this.formIsValid = true; // 지난 유효성 검사 이후에도 여전히 유효한지 확인
+
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.rate.val || this.rate.val < 0) {
+        // null or 음수인지 확인
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.areas.val.length === 0) {
+        // 전문분야가 적어도 하나 있어야함
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      }
+    },
     submitForm() {
+      this.validateForm(); // 폼 유효성 검사 실행
+
+      if (!this.formIsValid) return; // 유효성 검사 실패 시
+
       const formData = {
-        first: this.firstName,
-        last: this.lastName,
-        desc: this.description,
-        rate: this.rate,
-        areas: this.areas,
+        first: this.firstName.val,
+        last: this.lastName.val,
+        desc: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val,
       };
 
       console.log('formData: ', formData);
@@ -115,7 +210,8 @@ h3 {
 }
 
 /* 양식 검증 스타일링 */
-.invalid label {
+.invalid label,
+.invalid p {
   color: red;
 }
 
