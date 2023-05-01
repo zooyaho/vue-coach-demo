a169a63a30e283ac6284739c42ac6998ff8ff1ea
zooyaho <jiwoo1268@naver.com>
2023-05-01 17:52:27 +0900

    freat: 코치 가입 폼 생성

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,17 @@
 <template>
-  <h2>코치 등록 페이지</h2>
+  <base-card>
+    <section>
+      <h2>지금 바로 코치로 가입하세요!</h2>
+      <CoachForm></CoachForm>
+    </section>
+  </base-card>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
