6b98eb6d4a08fea9740aa41cc77a90e1ae65d03f
zooyaho <jiwoo1268@naver.com>
2023-05-02 09:11:28 +0900

    feat: 코치 등록 firebase연결

--- src/store/coaches/actions.js
+++ src/store/coaches/actions.js
@@ -1,13 +1,33 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
       hourlyRate: payload.rate,
       areas: payload.areas,
     };
-    context.commit('registerCoach', coachData);
+
+    // 가입 후 코치 등록 flow
+    // 코치 ID는 코치로 등록 하기 전부터 존재한다는 가정으로, PUT을 요청한다.
+    const response = await fetch(
+      `https://vue-http-b19e2-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!responseData.ok) {
+      // error...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
