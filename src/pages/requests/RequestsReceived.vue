330a99889d3ae0dfe8d64a9c7eed83a53f370b15
zooyaho <jiwoo1268@naver.com>
2023-05-02 15:39:43 +0900

    feat: request데이터 firebase연결

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <base-dialog :show="!!error" title="오류 발생" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>받은 요청</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
         <requests-item
           v-for="request in receivedRequests"
           :key="request.id"
@@ -24,6 +28,12 @@ export default {
   components: {
     RequestsItem,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
@@ -31,6 +41,23 @@ export default {
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
     },
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || '에러 발생!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+  created() {
+    this.loadRequests();
   },
 };
 </script>
--- src/store/requests/actions.js
+++ src/store/requests/actions.js
@@ -1,11 +1,56 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+
+    const res = await fetch(
+      `https://vue-http-b19e2-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    if (!res.ok) {
+      // error...
+      const error = new Error(resData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const resData = await res.json();
+
+    newRequest.id = resData.name; // firebase 자동 id 생성 뒤 반환
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const res = await fetch(
+      `https://vue-http-b19e2-default-rtdb.firebaseio.com/requests/${coachId}.json`
+    );
+    const resData = await res.json();
+
+    if (!res.ok) {
+      // error...
+      const error = new Error(resData.message || '오류 발생!!');
+      throw error;
+    }
+
+    const requests = [];
+
+    for (const key in resData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: resData[key].userEmail,
+        message: resData[key].message,
+      };
+
+      requests.push(request);
+    }
+    context.commit('setRequests', requests);
   },
 };
